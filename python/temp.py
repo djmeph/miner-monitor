3036150ad70e94c2f1a441e674b386980d4edd55
Norman Witte III <norm.witte@gmail.com>
2018-01-14 14:46:40 -0500

    dump json to console

--- python/temp.py
+++ python/temp.py
@@ -5,6 +5,7 @@ import Adafruit_DHT
 import re
 import time
 import argparse
+import json
 
 from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
@@ -19,9 +20,8 @@ def demo(n, block_orientation, rotate):
   device = max7219(serial, cascaded=n or 1, block_orientation=block_orientation, rotate=rotate or 0)
   Humidity, Celsius = Adafruit_DHT.read_retry(11, 4)
   Fahrenheit = 9.0/5.0 * Celsius + 32
+  print json.dumps({ 'temp': Fahrenheit, 'humidity': Humidity })
   msg = 'Temp: {0:0.1f} F  Humidity: {1:0.1f} %'.format(Fahrenheit, Humidity)
-  json = '{"temp":{0:0.1f},"humidity":{1:0.1f}}'.format(Fahrenheit, Humidity)
-  print json
   show_message(device, msg, fill="white", font=proportional(CP437_FONT))
 
 if __name__ == "__main__":
