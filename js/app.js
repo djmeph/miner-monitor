079630d76f1c880437fe5549063d8fd627846acc
Norman Witte III <norm.witte@gmail.com>
2018-01-15 22:35:03 -0500

    Pad output to 1 decimal

--- js/app.js
+++ js/app.js
@@ -4,7 +4,10 @@
   var socket = io.connect();
 
   socket.on('temp', function (data) {
-    console.log(data);
+    console.log({
+      temp: parseFloat(data.temp).toFixed(2),
+      humidity: parseFloat(data.humidity).toFixed(2)
+    });
   });
 
 })();
