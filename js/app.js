d90dd01f75ba7294a3bb22649c5840f60df076ae
Norman Witte III <norm.witte@gmail.com>
2018-01-15 22:24:54 -0500

    Client joins channel, socket emits data, client displays data on receive

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@
 
   var socket = io.connect();
 
+  socket.emit('join');
 
+  socket.on('temp', function (data) {
+    console.log(data);
+  });
 
 })();
--- sockets/temp.js
+++ sockets/temp.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const inspect = require('util').inspect;
-const opts = { colors: true, depth: Infinity };
-
 const path = require('path');
 const python = require('python-shell');
 const options = {
@@ -10,25 +7,27 @@ const options = {
   scriptPath: path.join(__dirname, '..', 'python')
 }
 
-tempInterval();
-
 module.exports = Socket;
 
 function Socket (io) {
-  io.on('connection', function (socket) {
-    console.log(socket.id);
-  });
-}
 
-function tempInterval () {
-  python.run('../python/temp.py', options, function (err, results) {
-    if (err || results.length == 0) {
-      console.error(err);
-      setTimeout(tempInterval, 5000);
-    } else {
-      var json = JSON.parse(results[0]);
-      console.log(inspect(json, opts));
-      setTimeout(tempInterval, 100);
-    }
+  io.on('join', function (socket) {
+    socket.join('miner-monitor');
   });
+
+  tempInterval();
+
+  function tempInterval () {
+    python.run('../python/temp.py', options, function (err, results) {
+      if (err || results.length == 0) {
+        console.error(err);
+        setTimeout(tempInterval, 5000);
+      } else {
+        var json = JSON.parse(results[0]);
+        io.to('miner-monitor').emit('temp', json);
+        setTimeout(tempInterval, 100);
+      }
+    });
+  }
+
 }
